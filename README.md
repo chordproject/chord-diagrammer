87f4852eb898a4f6b04977c7920de1e8f99b32df
Miguel Forget <tictactiptop@hotmail.com>
2022-06-23 13:04:36 +0200

    Update package.json and README

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
-# ChordProject Diagrammer
+# Chord Diagrammer
 
-A TypeScript frontend library for generating SVG chord diagrams from JSON specification.
+A TypeScript library to generate SVG chord diagrams.
 
 Inspired by: https://github.com/tombatossals/react-chords.
 
+Part of [ChordProject](https://chordproject.com/)
 ## Overview
 
-Generates SVG chord diagrams according to received specification.
+Generates SVG chord diagrams according to received specifications.
 
 ![Diagrams](./diagrammer.png)
 
 ## Usage
+
+`Chord Diagrammer` is on npm. To install run:
+
+```sh
+$ npm i chordproject-editor
+```
 
 It's really easy to draw an SVG chord diagram:
 
@@ -37,11 +44,54 @@ document.body.appendChild(svg); // add the svg in the html content (here the bod
 
 A ChordDiagram is defined by:
 
--   frets (array of numbers). Use 0 for open strings and -1 for muted strings. Frets must be relative to the base-fret.
--   fingers (array of numbers). Use 0 for no finger. The fingers array length must match the frets array length. The fingers are optional
--   base-fret (number)
+-   **frets** (array of numbers). Use 0 for open strings and -1 for muted strings. Frets must be relative to the base-fret.
+-   **fingers** (array of numbers). Use 0 for no finger. The fingers array length must match the frets array length. The fingers are optional
+-   **base-fret** (number)
 
-You can customize the diagram by passing a Settings object in the Diagrammer constructor.
-All of the settings has already a default value.
+## Customization
+You can customize the diagram by changing the default settings.
 
-#### Part of [ChordProject](https://chordproject.com/)
+For example:
+```ts
+builder.settings.dot.radius = 5; // change the dot radius
+builder.settings.neck.lineWidth = 0.8; //change the line width of the neck
+```
+Here is all the available settings:
+- dot:
+  - radius
+  - borderWith
+  - openStringRadius
+  - stringInfoMargin
+- neck
+  - lineWidth
+  - baseFretMargin
+  - useRoman
+  - nutWidth
+  - stringNameVisible
+  - stringNameMargin
+
+## Demo
+
+1. Clone
+2. Install dependencies:
+
+```sh
+$ npm i
+```
+
+1.  Run in dev mode:
+
+```sh
+$ npm run start
+```
+
+Open a browser and navigate to http://localhost:8082/ to load the demo.
+
+## Contributing
+
+This project welcomes contributions of all types. If you find any bug or want some new features, please feel free to create an issue or submit a pull request.
+
+Join the community and chat with us on **[Discord](https://discord.gg/ZQAgwBC9c8)**
+
+## License
+[MIT License](LICENSE)
--- src/svgBuilder.ts
+++ src/svgBuilder.ts
@@ -7,11 +7,7 @@ import { Dot } from "./dot";
 import { Settings } from "./settings";
 
 export class SvgBuilder {
-    private _settings: Settings = new Settings();
-
-    set settings(value: Settings) {
-        this._settings = value;
-    }
+    settings: Settings = new Settings();
 
     build(chord: ChordDiagram, instrument: Instrument): SVGElement {
         var tuning = instrument.tuning;
@@ -20,21 +16,21 @@ export class SvgBuilder {
         var baseFret = chord.baseFret > 0 ? chord.baseFret : 1;
 
         const baseBoxWidth =
-            (stringsCount - 1) * this._settings.stringSpace +
-            2 * (this._settings.dot.radius + this._settings.dot.borderWidth);
-        const baseFretTextWidth = baseFret <= 1 ? 0 : 9 + this._settings.neck.baseFretMargin;
+            (stringsCount - 1) * this.settings.stringSpace +
+            2 * (this.settings.dot.radius + this.settings.dot.borderWidth);
+        const baseFretTextWidth = baseFret <= 1 ? 0 : 9 + this.settings.neck.baseFretMargin;
         const viewBoxWidth = baseBoxWidth + baseFretTextWidth;
 
-        const baseBoxHeight = fretsOnChord * this._settings.fretSpace;
-        const stringNamesHeight = !this._settings.neck.stringNameVisible
+        const baseBoxHeight = fretsOnChord * this.settings.fretSpace;
+        const stringNamesHeight = !this.settings.neck.stringNameVisible
             ? 0
-            : 10 + this._settings.neck.stringNameMargin;
-        const nutHeight = baseFret > 1 ? 0 : this._settings.neck.nutWidth;
+            : 10 + this.settings.neck.stringNameMargin;
+        const nutHeight = baseFret > 1 ? 0 : this.settings.neck.nutWidth;
         const stringInfoHeight =
             4 +
-            this._settings.dot.openStringRadius +
-            this._settings.dot.stringInfoMargin +
-            this._settings.dot.borderWidth;
+            this.settings.dot.openStringRadius +
+            this.settings.dot.stringInfoMargin +
+            this.settings.dot.borderWidth;
         const viewBoxHeight = baseBoxHeight + stringNamesHeight + nutHeight + stringInfoHeight;
 
         var svg = Helper.createSVGElement("svg", {
@@ -44,28 +40,28 @@ export class SvgBuilder {
             viewBox: `0 0 ${viewBoxWidth} ${viewBoxHeight}`,
         });
 
-        const translateX = baseFretTextWidth + this._settings.dot.radius;
+        const translateX = baseFretTextWidth + this.settings.dot.radius;
         const translateY = nutHeight + stringInfoHeight;
         var rootElement = Helper.createSVGElement("g", {
             transform: `translate(${translateX}, ${translateY})`,
         });
 
         // Neck
-        const neck = new Neck(this._settings);
+        const neck = new Neck(this.settings);
         rootElement.appendChild(neck.build(tuning, stringsCount, fretsOnChord, baseFret));
 
         // Barres
         if (chord.barres.length > 0) {
             const barresData = chord.barres;
             barresData.forEach((barreData) => {
-                const barre = new Barre(this._settings);
+                const barre = new Barre(this.settings);
                 rootElement.appendChild(barre.build(barreData.fret, barreData.startString, barreData.endString));
             });
         }
 
         // Dots
         this.onlyDots(chord).forEach((dotData) => {
-            const dot = new Dot(this._settings);
+            const dot = new Dot(this.settings);
             const finger = chord.fingers ? chord.fingers[dotData.fret] : 0;
             rootElement.appendChild(dot.build(dotData.fret, dotData.value, finger, chord.baseFret === 1));
         });
